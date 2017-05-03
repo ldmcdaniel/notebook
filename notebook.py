05b1a649b14e7c9d226f4c2b8de4924736c645e3
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2017-05-03 13:12:40 -0500

    Added menu and ability to navigate the program in the console

--- notebook.py
+++ notebook.py
@@ -20,20 +20,24 @@ class Notebook:
     def __init__(self):
         self.notes = []
 
+    def _find_note(self, note_id):
+        for note in self.notes:
+            if str(note.id) == str(note_id):
+                return note
+        return None
+
     def new_note(self, memo, tags=''):
         self.notes.append(Note(memo, tags))
 
     def modify_memo(self, note_id, memo):
-        for note in self.notes:
-            if note.id == note_id:
-                note.memo = memo
-                break
+        note = self._find_note(note_id)
+        if note:
+            note.memo = memo
+            return True
+        return False
 
     def modify_tags(self, note_id, tags):
-        for note in self.notes:
-            if note.id == note_id:
-                note.tags = tags
-                break
+        self._find_note(note_id).tags = tags
 
     def search(self, filter):
         return [note for note in self.notes if note.match(filter)]
